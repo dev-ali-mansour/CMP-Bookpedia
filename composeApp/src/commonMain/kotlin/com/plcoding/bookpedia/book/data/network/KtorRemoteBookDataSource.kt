d664582b80d8fd774e57d2695b160e8f504d9563
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-08 14:55:39 +0300

    feat:Implement KtorRemoteBookDataSource
    This commit introduces the `KtorRemoteBookDataSource` class, which implements the `RemoteBookDataSource` interface.
    
    Key features:
    - It uses Ktor `HttpClient` to make network requests to the Open Library API.
    - The `searchBooks` suspend function takes a query string and an optional result limit.
    - It makes a GET request to `https://openlibrary.org/search.json`.
    - Query parameters include the search query (`q`), result limit (`limit`), language (`eng`), and specific fields to retrieve for each book.
    - The function utilizes a `safeCall` utility to handle potential network errors and returns a `Result` containing either a `SearchResponseDto` or a `DataError.Remote`.

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -0,0 +1,35 @@
+package com.plcoding.bookpedia.book.data.network
+
+import com.plcoding.bookpedia.book.data.dto.SearchResponseDto
+import com.plcoding.bookpedia.book.data.dto.SearchedBookDto
+import com.plcoding.bookpedia.core.data.safeCall
+import com.plcoding.bookpedia.core.domain.DataError
+import com.plcoding.bookpedia.core.domain.Result
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+
+private const val BASE_URL = "https://openlibrary.org"
+
+class KtorRemoteBookDataSource(
+    private val httpClient: HttpClient
+): RemoteBookDataSource {
+   override suspend fun searchBooks(
+        query: String,
+        resultLimit: Int?
+    ): Result<SearchResponseDto, DataError.Remote> {
+        return safeCall {
+            httpClient.get(
+                urlString = "$BASE_URL/search.json",
+            ) {
+                parameter("q", query)
+                parameter("limit", resultLimit)
+                parameter("language", "eng")
+                parameter(
+                    "fields",
+                    "key,title,author_name,author_key,cover_i,cover_edition_key,first_publish_year,rating_average,rating_count,number_of_pages_median,edition_count"
+                )
+            }
+        }
+    }
+}
