2e65abe9775f961bc0cd27d38719b481efec1108
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-09 13:18:33 +0300

    feat:Add book description fetching to repository
    This commit introduces the ability to fetch a book's description.
    
    - The `BookRepository` interface now includes a `getBookDescription` suspend function. This function takes a `bookId` (String) and returns a `Result` containing either the book's description (String?) or a `DataError`.
    - The `DefaultBookRepository` implements the `getBookDescription` function. It calls the `getBookDetails` method on the `remoteBookDataSource` and maps the result to extract the description.

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/BookRepository.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/BookRepository.kt
@@ -4,7 +4,6 @@ import com.plcoding.bookpedia.core.domain.DataError
 import com.plcoding.bookpedia.core.domain.Result
 
 interface BookRepository {
-    suspend fun searchBooks(
-        query: String
-    ): Result<List<Book>, DataError.Remote>
+    suspend fun searchBooks(query: String): Result<List<Book>, DataError.Remote>
+    suspend fun getBookDescription(bookId: String): Result<String?, DataError>
 }
