6aedec0c20382dc0957ee2b20387a508892852ba
Ali Mansour <dev.ali.mansour@gmail.com>
2025-07-09 17:42:18 +0300

    refactor:Rename AppDatabaseConstructor to BookDatabaseConstructor
    This commit renames the `AppDatabaseConstructor` object to `BookDatabaseConstructor` in `BookDatabaseConstructor.kt`.

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/BookDatabaseConstructor.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/BookDatabaseConstructor.kt
@@ -3,6 +3,6 @@ package com.plcoding.bookpedia.book.data.database
 import androidx.room.RoomDatabaseConstructor
 
 @Suppress("NO_ACTUAL_FOR_EXPECT")
-expect object AppDatabaseConstructor : RoomDatabaseConstructor<FavoriteBookDatabase> {
+expect object BookDatabaseConstructor : RoomDatabaseConstructor<FavoriteBookDatabase> {
     override fun initialize(): FavoriteBookDatabase
 }
